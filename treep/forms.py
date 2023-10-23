5e94e8e3e6b803cc452178994e91ac87fca993b5
Luis Fernando <97605117+LFBG96@users.noreply.github.com>
2023-10-22 23:44:05 -0300

    validação data backend

--- treep/forms.py
+++ treep/forms.py
@@ -1,5 +1,9 @@
 from django import forms
 from .models import Roteiro,Natureza, Gastronomia,Praia,Cultura 
+from django.utils import timezone as tz
+from django.utils import datetime_safe
+from django.core.exceptions import ValidationError
+from datetime import datetime
 
 class RoteiroForm(forms.ModelForm):
     
@@ -7,13 +11,24 @@ class RoteiroForm(forms.ModelForm):
     gastronomia = forms.ModelMultipleChoiceField(queryset=Gastronomia.objects.all(), widget=forms.CheckboxSelectMultiple,required=False)
     praia = forms.ModelMultipleChoiceField(queryset=Praia.objects.all(),widget=forms.CheckboxSelectMultiple,required=False)
     cultura = forms.ModelMultipleChoiceField(queryset=Cultura.objects.all(),widget=forms.CheckboxSelectMultiple,required=False)
-    """
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields["gastronomia"].widget.attrs.update({"class": "special"})
-        self.fields["gastronomia"].widget.attrs.update({"style": "display: none;"})
+    def clean(self, *args, **kwargs):
+    
+        cleaned_data =  super().clean(*args, **kwargs)
+
+        data = cleaned_data.get('data')
+
+        # Obter a data atual
+        data_atual = datetime.today()
+
+        # Formatar a data para mostrar apenas o ano, mês e dia
+        data_formatada = data_atual.strftime('%Y-%m-%d')
+
+        print(data_formatada)
+
+        if str(data) < data_formatada:
+            raise ValidationError('Start time must be later than now.')
+
         
-        """
     class Meta:
         model = Roteiro
         fields = '__all__'
