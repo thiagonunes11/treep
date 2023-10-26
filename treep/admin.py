11a38c33dff90e6081546f523fafb31c5427f83f
Luis Fernando <97605117+LFBG96@users.noreply.github.com>
2023-10-26 12:53:11 -0300

    Update admin.py

--- treep/admin.py
+++ treep/admin.py
@@ -3,10 +3,22 @@ from .models import *
 
 # Register your models here.
 
-admin.site.register(Natureza)
-admin.site.register(Gastronomia)
-admin.site.register(Praia)
-admin.site.register(Cultura)
+class Apagar(admin.ModelAdmin):
+    def delete_model(self, request, obj):
+        obj.img.delete(save=True)
+        obj.delete()
+         
+
+    def delete_queryset(self, request, queryset):
+
+        for obj in queryset.all():
+            obj.img.delete(save=True)
+        queryset.delete()
+
+admin.site.register(Natureza,Apagar)
+admin.site.register(Gastronomia,Apagar)
+admin.site.register(Praia,Apagar)
+admin.site.register(Cultura,Apagar)
 
 #admin.site.register(Roteiro)
 
