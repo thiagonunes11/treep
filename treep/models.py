ac3446197c385207158f7a792357d3929548dc0d
lfbg96 <lf5154223@gmail.com>
2023-10-27 10:10:26 -0300

    alterações css e media

--- treep/models.py
+++ treep/models.py
@@ -4,27 +4,29 @@ from django.contrib.auth.models import User
 from django.utils.timezone import datetime
 
 
-
-
 # Create your models here.
 
 class Natureza(models.Model):
     nome = models.CharField(max_length=200)
     descricão = models.TextField(max_length=500)
-    img = models.ImageField(upload_to="imagem_natureza")    
+    img = models.ImageField(upload_to="imagem_natureza")
     cidade = models.CharField(max_length=200)
     url = models.URLField(blank=True)
+
     def __str__(self):
         return self.nome
+
 
 class Gastronomia(models.Model):
     nome = models.CharField(max_length=200)
     descricão = models.TextField(max_length=500)
-    img = models.ImageField(upload_to="imagem_gastronomia")
+    img = models.ImageField(upload_to="imagem_gastronomia/")
     cidade = models.CharField(max_length=200)
     url = models.URLField(blank=True)
+
     def __str__(self):
         return self.nome
+
 
 class Praia(models.Model):
     nome = models.CharField(max_length=200)
@@ -32,8 +34,10 @@ class Praia(models.Model):
     img = models.ImageField(upload_to="imagem_praia")
     cidade = models.CharField(max_length=200)
     url = models.URLField(blank=True)
+
     def __str__(self):
         return self.nome
+
 
 class Cultura(models.Model):
     nome = models.CharField(max_length=200)
@@ -41,20 +45,20 @@ class Cultura(models.Model):
     img = models.ImageField(upload_to="imagem_cultura")
     cidade = models.CharField(max_length=200)
     url = models.URLField(blank=True)
+
     def __str__(self):
         return self.nome
-
 
 
 class Roteiro(models.Model):
     nome = models.CharField(max_length=100)
     data = models.DateField(default=django.utils.timezone.now)
-    natureza = models.ManyToManyField(Natureza, blank=True)    
-    gastronomia = models.ManyToManyField(Gastronomia, blank=True)    
-    praia = models.ManyToManyField(Praia,blank=True)    
-    cultura = models.ManyToManyField(Cultura,blank=True)
-    usuario = models.ForeignKey(User, null=True, blank=True,on_delete=models.PROTECT)
+    natureza = models.ManyToManyField(Natureza, blank=True)
+    gastronomia = models.ManyToManyField(Gastronomia, blank=True)
+    praia = models.ManyToManyField(Praia, blank=True)
+    cultura = models.ManyToManyField(Cultura, blank=True)
+    usuario = models.ForeignKey(
+        User, null=True, blank=True, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.nome
-    
